89c0e78a39f648be03b026800f666d8eef2132d5
andrey <andrey.homenko@requestum.com>
2019-05-27 13:50:29 +0300

    Finished 10 online lessons by Gosha Dudar.

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <Example></Example>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Example from './components/Example.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Example
   }
 }
 </script>
@@ -21,8 +21,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ project-1/src/components/Example.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <app-input :msg="message" @messChange="message = $event"></app-input>
+  </div>
+</template>
+
+<script>
+  import Input from './Field.vue';
+
+  export default {
+    data() {
+      return {
+        message: 'Test text'
+      }
+    },
+    components: {
+      'app-input': Input
+    }
+  }
+</script>
